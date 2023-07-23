d63ed03713621c2d367fe6d953ba157a7283e75c
Sergey Lozhenkov <s-lozhenkov@yandex.ru>
2023-07-23 20:18:29 +0500

    upd_debug

--- lesson1C#/Program.cs
+++ lesson1C#/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2)
+{
+    Console.WriteLine($" Max = {num2} and Min = {num1} ");
+}
+else
+{
+    Console.WriteLine($" Max = {num1} and Min = {num2} ");
+}
+*/
+
