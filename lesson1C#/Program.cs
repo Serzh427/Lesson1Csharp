6ad31c10840e7fc0beb47cd740e58f3380d80213
Sergey Lozhenkov <s-lozhenkov@yandex.ru>
2023-07-27 15:50:37 +0500

    Task4

--- lesson1C#/Program.cs
+++ lesson1C#/Program.cs
@@ -57,3 +57,14 @@ else
     Console.WriteLine("This number is odd");
 }
 */
+
+/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+*/
+Console.Write("Input any number: ");
+int num = Convert.ToInt32.(Console.ReadLine());
+
+while ( num % 2 == 0 )
+{
+    Console.Write( num + " " )
+    num++;
+}
