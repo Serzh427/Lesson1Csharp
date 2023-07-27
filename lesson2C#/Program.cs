e4c20aed1f9fd8b4672d2c768f49026d47cd3d7c
Sergey Lozhenkov <s-lozhenkov@yandex.ru>
2023-07-27 22:04:57 +0500

    NewTasks

--- lesson2C#/Program.cs
+++ lesson2C#/Program.cs
@@ -12,7 +12,7 @@
 13254 % 10000 = 3254
 */
 
-// Task 1. Напишите программу, которая выводит случайное трёхзначное число и 
+/* Task 1. Напишите программу, которая выводит случайное трёхзначное число и 
 // удаляет вторую цифру этого числа.
 
 int CutNumber(int num)
@@ -30,8 +30,52 @@ int randNumber = new Random().Next(100, 1000);
 
 int newNumber = CutNumber(randNumber);
 Console.WriteLine ($"New version of {randNumber} is {newNumber}");
+*/
 
+/* Task 2. Напишите программу, которая принимает на вход число 
+и проверяет, кратно ли оно одновременно a и b.
 
+bool isDividable(int num, int div1, int div2)
+{
+    //if ( num % div1 == 0 && num % div2 == 0)
+    //    return true;
+    //else
+    //    return false;
+     // *нижняя строка тоже самое что и верхняя*
+    return num % div1 == 0 && num % div2 == 0;       
+}
 
+Console.Write("Input a number for cheking: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a first divider: ");
+int divider1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second divider: ");
+int divider2 = Convert.ToInt32(Console.ReadLine());
 
+bool res = isDividable(number, divider1, divider2);
+if(res) Console.Write("Yes");
+else Console.Write("No");
+*/
+
+/* Task 3.  Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+и показывает наибольшую цифру числа.
+
+int MaxNum(int num);
+{
+    int hundreds = num % 10;
+    int units = num / 10;
+    if (units < hundreds)
+    return hundreds;
+    else
+    return units ;   
+}    
+int number = new Random().Next( 10, 99);
+Console.WriteLine(number);
+int result = MaxNum(number);
+Console.WriteLine(result);
+*/ 
+
+/* Task 4. Напишите программу, которая принимает на вход два числа и проверяет, 
+является ли одно число квадратом другого.
+*/
 
