8ac2f3c3d7341e956bc2f110030defcfbc55ad92
Sergey Lozhenkov <s-lozhenkov@yandex.ru>
2023-07-31 19:31:06 +0500

    task3

--- lesson3C#/Program.cs
+++ lesson3C#/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+int show2number ( int num )
+{
+    int thirdNumber = num % 100;
+    int secondNumber = thirdNumber / 10;
+    return secondNumber;
+}
+
+int randomizer = new Random().Next(100,999);
+int newNumber = show2number(randomizer);
+Console.WriteLine ($"New version of {randomizer} is {newNumber}");
+*/
+
+/* Задача 13: Напишите программу, 
+которая выводит третью цифру заданного числа
+ ( или -1, если третьей цифры нет).
+
+int needNumber ( int num )
+{
+    
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    return  num % 10;
+}
+int randomizer = new Random().Next();
+int newNumber = needNumber(randomizer);
+if ( randomizer < 100 )
+{
+    Console.WriteLine ($"New number is {randomizer} and answer is -1");
+}
+else
+{
+    Console.WriteLine ($"New version of {randomizer} is {newNumber} ");
+}
+*/
+
+/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+
+Console.Write("Input number of day week today: ");
+int validDay = Convert.ToInt32(Console.ReadLine());
+if ( validDay > 5 && validDay <=7 )
+    {
+        Console.WriteLine("выходной");
+    }
+    else
+    {
+         if ( validDay > 0 && validDay < 6)
+        {
+        Console.WriteLine("опять работа");
+        }
+        else
+        {
+        Console.WriteLine("укажите цифру от 1 до 7");
+        }  
+     
+    }      
+*/
