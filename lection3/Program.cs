09fc0a8f954580039273eee6ec7d533a5e163d52
Sergey Lozhenkov <s-lozhenkov@yandex.ru>
2023-08-07 22:50:56 +0500

    explanation

--- lection3/Program.cs
+++ lection3/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine ("Показывает текст всего лишь");
+}
+Method1(); // вызвать метод
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+
+
+// Вид 21
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+
+}
+Method21(msg: "Текст", count: 4); // Method21( count: 4, msg: "Текст");
+
+// Вид3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year)
+
+// Вид4
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = string.Empty;
+
+    while ( i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
+
