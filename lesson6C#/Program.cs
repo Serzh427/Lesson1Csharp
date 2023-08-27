551af5aff11b65f8ef8df159e54d507011c791f2
Sergey Lozhenkov <s-lozhenkov@yandex.ru>
2023-08-27 18:42:36 +0500

    task

--- lesson6C#/Program.cs
+++ lesson6C#/Program.cs
@@ -1,1 +1,152 @@
-﻿
+﻿/* 1. Напишите программу, которая перевернёт одномерный массив
+(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+int [] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int [] array = new int [size];
+
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(minValue,maxValue + 1);
+
+    return array;
+}
+
+void WriteArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+void ReverseArray ( int [] array)
+{
+    for ( int i = 0; i < array.Length / 2; i++ )
+    {
+        int temp = array [i];
+        array[i] = array [array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+void ReverseArray2 (int[] array)
+{
+    for (int i = 0, j = array.Length -1; i < j; i++, j--)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+Console.Write("Input a length of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, minValue, maxValue);
+WriteArray(myArray);
+ReverseArray2(myArray);
+WriteArray (myArray);
+*/
+
+/* 2. Напишите программу, которая принимает на вход три числа и проверяет, 
+может ли существовать треугольник с сторонами такой длины.
+
+
+bool triangle (int a, int b, int c)
+{
+    return a < b + c && b < c + a && c < a + b;
+}
+
+Console.Write("Input length of side a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input length of side b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input length of side c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (triangle(a,b,c)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+*/
+
+/* 3. Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+Первые два числа Фибоначчи: 0 и 1.
+
+int [] CreateArray (int size)
+{
+    int [] array = new int [size];
+    array[0] = 0;
+    array[1] = 1;
+
+    for (int i = 2; i < array.Length; i++)
+        array[i] = array[i - 1] + array[i - 2];
+
+    return array;
+}
+
+void WriteArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+Console.Write("Input a length of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateArray(size);
+WriteArray(myArray);
+*/
+
+/* 4. Напишите программу, которая будет создавать копию заданного массива 
+с помощью поэлементного копирования.
+
+
+int [] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int [] array = new int [size];
+
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(minValue,maxValue + 1);
+
+    return array;
+}
+
+Console.Write("Input a length of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a min possible value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a max possible value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+void WriteArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(size, minValue, maxValue);
+WriteArray(myArray);
+
+int [] CopyArray ( int size, int [] firstArray )
+{
+    int [ ] secondArray = new int[size];
+    for ( int i = 0; i < size; i++)
+    {
+        secondArray[i] = firstArray [i];
+    }
+    return secondArray;
+}
+
+int [] CopyMyArray = CopyArray(size, myArray);
+WriteArray (CopyMyArray);
+
+myArray[0] =  0; // проверка, связаны ли два масива
+WriteArray(myArray);
+WriteArray (CopyMyArray);
+*/
