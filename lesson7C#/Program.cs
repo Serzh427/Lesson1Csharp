f3cc4da8dc69c580c496ca2f05f39e763a11da83
Sergey Lozhenkov <s-lozhenkov@yandex.ru>
2023-09-10 02:27:25 +0500

    tasks7

--- lesson7C#/Program.cs
+++ lesson7C#/Program.cs
@@ -1,6 +1,6 @@
 ﻿/* Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
 
-int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+int[,] CreateRandom2dArray()
 {
     Console.Write("Input a quantity of rows: ");
     int rows = Convert.ToInt32(Console.ReadLine());
@@ -25,9 +25,8 @@ void Show2dArray(int [,] array)
 {
     for(int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j < array.GerLength(1); j++)
-            Console.WriteLine(array[i,j] + " ")
-        Console.WriteLine()
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
     }
 }
 
@@ -230,3 +229,166 @@ Console.WriteLine();
 SortLine(myArray);
 Show2dArray(myArray);
 */
+
+// Homework
+/*Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+double [,] CreateRandom2dArray()
+{   Console.Write("Input a quantity of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a quantity of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    double min = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    double max = Convert.ToDouble(Console.ReadLine());
+
+    double [,] array = new double [rows,columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = Math.Round((new Random().NextDouble()*(max - min) + min),2);
+
+        
+    return array;
+}
+
+void Show2dArray(double [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }
+}
+
+
+double[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+*/
+
+/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a quantity of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a quantity of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    int [,] array = new int [rows,columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array [i,j] = new Random().Next(min, max + 1);
+
+        
+    return array;
+}
+void Show2dArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }
+   
+}
+
+Console.WriteLine("x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [x,y] ;
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+
+
+for(int i = 0; i < myArray.GetLength(0); i++)
+    {
+        
+        for(int j = 0; j < myArray.GetLength(1); j++)
+        {
+            if (i == x && j == y)
+            {
+
+                Console.WriteLine($"This is a {myArray[x,y]}");
+            }
+            else
+            {
+        
+            }
+
+        }    
+    }
+ */  
+
+/* Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a quantity of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a quantity of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    int [,] array = new int [rows,columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array [i,j] = new Random().Next(min, max + 1);
+
+        
+    return array;
+}
+
+void Show2dArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }
+}
+
+double result (int [,] array)
+{
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {   
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                sum += array[i, j];
+            }
+            Console.Write($"{ sum / array.GetLength(0)} ");
+        }
+    }   return sum / array.GetLength(0);
+    
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+
+Console.WriteLine(result(myArray));
+*/
+
+
+
